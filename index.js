133a858e71d85d3f729ead60fec067b24495c6ec
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-03-04 08:21:12 +0600

    something

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const loadLessons=()=>{
 fetch('https://openapi.programming-hero.com/api/levels/all')///return promise
 .then(res=> res.json()) // promise json data
-.then(data=> displayLessons(data))
+.then(json=> displayLessons(json.data))
+}
+const loadLevelWord=(id)=>{
+    console.log(id)
 }
 const displayLessons=(lessons)=>{
 // 1. get the container & empty
@@ -9,18 +12,19 @@ const levelContainer=document.getElementById('level-container');
 levelContainer.innerHTML="";
 console.log(levelContainer)
 // 2. get into every lesson
-forEach(lesson=>{
-console.log(lesson)
-})
-
+lessons.forEach(lesson=>{
+// console.log(lesson)
 // 3.Create new element
 const btnDiv=document.createElement('div')
-btnDiv.innerHTML=`<button  class="">
-<i class="fa-solid fa-book-open"></i>Lesson
+btnDiv.innerHTML=`<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+<i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}
 </button>
 `
 // 4. Append Into Container
 levelContainer.append(btnDiv)
+})
+
+
 }
 
 
